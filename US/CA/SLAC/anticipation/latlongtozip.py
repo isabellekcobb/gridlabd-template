eba9ce5458cf8d9fd16e5928dcf03f541dd61d44
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-17 16:27:24 -0700

    Update latlongtozip.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/latlongtozip.py
+++ US/CA/SLAC/anticipation/latlongtozip.py
@@ -5,9 +5,9 @@ import subprocess
 latitude = pd.read_csv('latitude.csv')['latitude'].tolist()
 longitude = pd.read_csv('longitude.csv')['longitude'].tolist()
 
-command = 'gridlabd geodata merge -D census {} -o $OPENFIDO_OUTPUT/test_zipcode.csv'
+command = 'gridlabd geodata merge -D census {} -o $OPENFIDO_OUTPUT/test_zipcode.csv -a'
 
 # Iterate over latitude and longitude pairs and execute the census subcommand for each
 for lat, lon in zip(latitude, longitude):
     formatted_command = command.format(f'{lat},{lon}')
-    subprocess.run(formatted_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    subprocess.run(formatted_command, shell=True)

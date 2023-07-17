3863c9516c068ed045b6adaa1750bca6bfd1acd1
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-17 15:52:25 -0700

    Update latlongtozip.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/latlongtozip.py
+++ US/CA/SLAC/anticipation/latlongtozip.py
@@ -1,26 +1,13 @@
 import requests
 import pandas as pd
 
-def get_zipcode(latitude, longitude, api_key):
-    url = f'https://api.opencagedata.com/geocode/v1/json?q={latitude}%2C{longitude}&key={api_key}'
-    response = requests.get(url)
-    data = response.json()
-
-    # Check if the API request was successful
-    if response.status_code == 200 and data.get('results'):
-        # Extract the zip code from the API response
-        zipcode = data['results'][0]['components']['postcode']
-        return zipcode
-    else:
-        # Handle the error case
-        error_message = data.get('status', 'Unknown error')
-        print(f'Error: {error_message}')
-        return None
-
-# Provide your latitude, longitude, and API key
+# Provide your latitude, longitude
 latitude = pd.read_csv('latitude.csv')
 longitude = pd.read_csv('longitude.csv')
-api_key = '580e79eccf33456f9cc0328c5729fa25'
 
-zipcode = get_zipcode(latitude, longitude, api_key)
-zipcode.to_csv('test_zipcode.csv', index=False)
+command = 'gridlabd geodata merge -D census {} -o $OPENFIDO_OUTPUT/test_zipcode.csv'
+
+# Iterate over latitude and longitude pairs and execute the census subcommand for each
+for lat, lon in zip(latitude, longitude):
+    formatted_command = command.format(f'{lat},{lon}')
+    subprocess.run(formatted_command, shell=True)

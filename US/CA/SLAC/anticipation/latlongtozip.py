1a787f190244b7bc90e29b53731c52032cc575df
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-17 14:21:06 -0700

    Update latlongtozip.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/latlongtozip.py
+++ US/CA/SLAC/anticipation/latlongtozip.py
@@ -23,7 +23,6 @@ longitude = -122.4194
 api_key = '580e79eccf33456f9cc0328c5729fa25'
 
 zipcode = get_zipcode(latitude, longitude, api_key)
-//zipcode.to_csv('test_zipcode.csv', index=False)
 
 zipCSV = pd.read_csv(pd.compat.StringIO(zipcode))
 zipCSV.to_csv('test_zipcode.csv', index=False)

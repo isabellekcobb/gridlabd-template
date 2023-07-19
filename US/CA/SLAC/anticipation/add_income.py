93d004263d557ae31b331f32c9ae8802a32c4dad
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-19 10:16:57 -0700

    Update add_income.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/add_income.py
+++ US/CA/SLAC/anticipation/add_income.py
@@ -3,33 +3,25 @@ import pandas as pd
 from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut, GeocoderUnavailable
 import time
-import requests
 
-def get_zipcode(lat, lon, api_key):
-    base_url = "http://dev.virtualearth.net/REST/v1/Locations"
-    query_params = {
-        "key": api_key,
-        "includeEntityTypes": "Postcode1",
-        "c": f"{lat},{lon}",
-        "o": "json"
-    }
+def get_zipcode(lat, lon):
+    geolocator = Nominatim(user_agent="zipcode_converter", timeout=10)  # Increased timeout to 10 seconds
+    retries = 5  # Maximum number of retries
+    location = None
     
-    try:
-        response = requests.get(base_url, params=query_params)
-        response.raise_for_status()
-        data = response.json()
-        if "resourceSets" in data and data["resourceSets"]:
-            results = data["resourceSets"][0]["resources"]
-            if results:
-                address = results[0]["address"]
-                if "postalCode" in address:
-                    return address["postalCode"]
-    except requests.exceptions.RequestException as e:
-        print(f"Error occurred: {e}")
+    for _ in range(retries):
+        try:
+            location = geolocator.reverse((lat, lon), exactly_one=True)
+            if location is not None and 'address' in location.raw:
+                address = location.raw['address']
+                if 'postcode' in address:
+                    return address['postcode']
+        except (GeocoderTimedOut, GeocoderUnavailable):
+            # Retry after a delay
+            time.sleep(2)
     
     return ""
 
-bing_maps_api_key = "AiCmdQvtGYgBidB1BVvGfHPIGrxLwGQ0K8fAeWFRNVZqsqtXLoZgRmbw6V-ZTPiA"
 
 # Read latitude and longitude from CSV files
 latitude = pd.read_csv('latitude.csv')
@@ -41,15 +33,13 @@ coordinates = list(zip(latitude['latitude'], longitude['longitude']))
 # Convert coordinates to zip codes
 results = []
 for lat, lon in coordinates:
-    zipcode = get_zipcode(lat, lon, bing_maps_api_key)
+    zipcode = get_zipcode(lat, lon)
     if not zipcode:
         zipcode = 'N/A'  # Assign a default value for missing zip codes
     results.append((lat, lon, zipcode))
 
 # Create DataFrame from the results
 df = pd.DataFrame(results, columns=["latitude", "longitude", "zipcode"])
-
-df.to_csv('results.csv',index=False)
 
 # Read income data from CSV file
 income = pd.read_csv("income_CA.csv")

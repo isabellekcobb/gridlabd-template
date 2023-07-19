c6bc5c89d3f75effe3130bf11d8d8a70967e525f
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-19 10:36:27 -0700

    Update testing_subcommand.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/testing_subcommand.py
+++ US/CA/SLAC/anticipation/testing_subcommand.py
@@ -1,17 +1,22 @@
 import subprocess
+import csv
 
-def gridlabd_geodata_merge(location1, location2):
+def gridlabd_geodata_merge(location1, location2, output_file):
     cmd = f"gridlabd geodata merge -D distance location {location1} {location2}"
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     output, _ = process.communicate()
-    
+
     if process.returncode == 0:
-        return output.decode().strip()
+        with open(output_file, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Location 1", "Location 2", "Result"])
+            writer.writerow([location1, location2, output.decode().strip()])
+        print(f"Results written to '{output_file}' successfully.")
     else:
-        return ""
+        print("Error executing gridlabd geodata merge.")
 
 # Example usage:
 location1 = "37.4150,-122.2056"
 location2 = "37.3880,-122.2884"
-result = gridlabd_geodata_merge(location1, location2)
-print(result)
+output_file = "results.csv"
+gridlabd_geodata_merge(location1, location2, output_file)

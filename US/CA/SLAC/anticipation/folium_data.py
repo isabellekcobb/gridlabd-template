c48277415ff0a63a8a5b7cefa395aea6eea17a60
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-20 15:29:46 -0700

    Update folium_data.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/folium_data.py
+++ US/CA/SLAC/anticipation/folium_data.py
@@ -6,10 +6,11 @@ csv_input['status'] = 'OK'		# set default as OK
 strike_threshold = 0.1
 
 # read the income values
-income_in=pd.read_csv("merged_results.csv", usecols=[3])
+income_in=pd.read_csv("merged_results.csv", usecols=['median_all'])
+income_in.to_csv('median_income.csv', index=False)
 
 for index, row in csv_input.iterrows():
 	if row['strike'] > strike_threshold:
 		csv_input.loc[index, "status"] = 'FAILED'
-	csv_input.loc[index, "income"] = income_in[index]
+
 csv_input.to_csv('path_result_plot.csv', index=False)

a443d06c85f6e47716ee7498c47f0b0896501016
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-20 16:10:29 -0700

    Update folium_data.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/folium_data.py
+++ US/CA/SLAC/anticipation/folium_data.py
@@ -1,19 +1,17 @@
 import pandas as pd
 
-csv_input = pd.read_csv('path_result.csv', usecols=['position', 'latitude', 'longitude', 'linesag', 'linesway', 'contact', 'strike', 'income'])
+csv_input = pd.read_csv('path_result.csv', usecols=['position', 'latitude', 'longitude', 'linesag', 'linesway', 'contact', 'strike'])
 csv_input['status'] = 'OK'    # set default as OK
 
 # read the strike value 
 strike_threshold = 0.1
 
-#read the income values
+#load in the income values
 income_in=pd.read_csv('merged_results.csv', usecols=['median_all'])
+csv_input['income']=income_in
 
 for index, row in csv_input.iterrows():
     if row['strike'] > strike_threshold:
         csv_input.loc[index, "status"] = 'FAILED'
-    
-    # load the income column with the generated data based on pole location
-    csv_input.loc[index, "income"] = income_in[index]
 
 csv_input.to_csv('path_result_plot.csv', index=False)

6f1ce8d0c0217300a8180f88abbb7146753f2b63
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-20 15:47:53 -0700

    Update folium_data.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/folium_data.py
+++ US/CA/SLAC/anticipation/folium_data.py
@@ -1,13 +1,16 @@
 import pandas as pd
-csv_input = pd.read_csv('path_result.csv', \
-	usecols = ['position', 'latitude', 'longitude', 'linesag', 'linesway', 'contact', 'strike','income'])
-csv_input['status'] = 'OK'		# set default as OK
+
+csv_input = pd.read_csv('path_result.csv', usecols=['position', 'latitude', 'longitude', 'linesag', 'linesway', 'contact', 'strike', 'income'])
+csv_input['status'] = 'OK'    # set default as OK
+
 # read the strike value 
 strike_threshold = 0.1
 
 for index, row in csv_input.iterrows():
-	if row['strike'] > strike_threshold:
-		csv_input.loc[index, "status"] = 'FAILED'
-	csv_input.loc[index, "income"] = 'default'
+    if row['strike'] > strike_threshold:
+        csv_input.loc[index, "status"] = 'FAILED'
+    
+    # Set every value in the 'income' column to 'default'
+    csv_input.loc[index, "income"] = 'default'
 
 csv_input.to_csv('path_result_plot.csv', index=False)

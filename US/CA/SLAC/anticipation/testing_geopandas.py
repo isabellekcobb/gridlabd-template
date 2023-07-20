c782db099297288a82e3b0cb9c641d11e7016c73
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-20 10:31:46 -0700

    Update testing_geopandas.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/testing_geopandas.py
+++ US/CA/SLAC/anticipation/testing_geopandas.py
@@ -8,7 +8,7 @@ def get_zipcode_geopandas(lat, lon, shapefile_path):
     # Check if the point is within any polygon (zipcode boundary)
     for idx, row in gdf.iterrows():
         if point.within(row['geometry']):
-            return row['ZIP Code']
+            return row[{ZIP CODE}]
     
     return ""
 # Example usage:

06dc8248021e3127596a4cb807a321413635894a
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-20 16:00:42 -0700

    Update folium_data.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/folium_data.py
+++ US/CA/SLAC/anticipation/folium_data.py
@@ -6,11 +6,14 @@ csv_input['status'] = 'OK'    # set default as OK
 # read the strike value 
 strike_threshold = 0.1
 
+#read the income values
+income_in=pd.read_csv('merged_results.csv', usecols=['median_all'])
+
 for index, row in csv_input.iterrows():
     if row['strike'] > strike_threshold:
         csv_input.loc[index, "status"] = 'FAILED'
     
-    # Set every value in the 'income' column to 'default'
-    csv_input.loc[index, "income"] = 'default'
+    # load the income column with the generated data based on pole location
+    csv_input.loc[index, "income"] = income_in[index]
 
 csv_input.to_csv('path_result_plot.csv', index=False)

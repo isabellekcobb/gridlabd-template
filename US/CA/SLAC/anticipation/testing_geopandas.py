ee7f36eea578c317f2a4e76da32ef111b142080e
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-20 13:47:51 -0700

    Update testing_geopandas.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/testing_geopandas.py
+++ US/CA/SLAC/anticipation/testing_geopandas.py
@@ -1,20 +1,46 @@
 import geopandas as gpd
+import pandas as pd
 from shapely.geometry import Point
 
-def get_zipcode_geopandas(lat, lon, shapefile_path):
-    point = Point(lon, lat)
-    gdf = gpd.read_file(shapefile_path)
+def get_zipcode_geopandas(latitude_file, longitude_file, geojson_file, output_csv_file):
+    # Read latitude and longitude from CSV files
+    latitude_df = pd.read_csv(latitude_file)
+    longitude_df = pd.read_csv(longitude_file)
 
-    # Check if the point is within any polygon (zipcode boundary)
-    for idx, row in gdf.iterrows():
-        if point.within(row['geometry']):
-            return gdf['ZIP_CODE']
-    
-    return ""
-    
+    # Create a list of latitude and longitude pairs
+    coordinates = zip(latitude_df['latitude'], longitude_df['longitude'])
+
+    # Read the GeoJSON file into a GeoDataFrame
+    gdf = gpd.read_file(geojson_file)
+
+    zipcodes = []
+    # Iterate through each latitude and longitude pair
+    for lat, lon in coordinates:
+        point = Point(lon, lat)
+        zipcode = None
+
+        # Check if the point is within any polygon (zipcode boundary)
+        for idx, row in gdf.iterrows():
+            if point.within(row['geometry']):
+                zipcode = row['ZIP_CODE']
+                break
+
+        zipcodes.append(zipcode)
+
+    # Create a new DataFrame with zip codes
+    result_df = pd.DataFrame({'latitude': latitude_df['latitude'], 'longitude': longitude_df['longitude'], 'Zipcode': zipcodes})
+
+    # Write the zip codes to a CSV file
+    result_df.to_csv(output_csv_file, index=False)
+
 # Example usage:
-latitude = 37.41504514168805
-longitude = -122.2056472090359
-shapefile_path = "USA_ZIP_Code_Boundaries.geojson"
-zipcode = get_zipcode_geopandas(latitude, longitude, shapefile_path)
-print(f"Zip Code: {zipcode}")
+latitude_file = "latitude.csv"  # Replace with the path to your latitude CSV file
+longitude_file = "longitude.csv"  # Replace with the path to your longitude CSV file
+geojson_file = "USA_ZIP_Code_Boundaries.geojson"  # Replace with the path to your GeoJSON file
+output_csv_file = "output_zipcodes.csv"  # Replace with the desired output CSV file path
+
+get_zipcode_geopandas(latitude_file, longitude_file, geojson_file, output_csv_file)
+
+
+
+

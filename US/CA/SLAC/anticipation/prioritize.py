2a69ae04ab5fd942f543d5eb76d440dc73528c77
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-21 09:44:56 -0700

    Update prioritize.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/prioritize.py
+++ US/CA/SLAC/anticipation/prioritize.py
@@ -6,5 +6,8 @@ csv_input = pd.read_csv('path_result_plot.csv')
 # Filter rows with 'FAILED' status
 failed_rows = csv_input[csv_input['status'] == 'FAILED']
 
+# Sort the failed_rows based on 'strike' column in descending order
+failed_rows_sorted = failed_rows.sort_values(by='strike', ascending=False)
+
 # Save the filtered rows to a new CSV file
 failed_rows.to_csv('failed_results.csv', index=False)

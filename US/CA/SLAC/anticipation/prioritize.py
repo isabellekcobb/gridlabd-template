bec3df6e4034538709d70abb281d40fc6b8599f5
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-21 12:56:50 -0700

    Update prioritize.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/prioritize.py
+++ US/CA/SLAC/anticipation/prioritize.py
@@ -1,11 +1,17 @@
 import pandas as pd
 
 # Read the CSV file and use the 'status' column as the sort key
-csv_input = pd.read_csv('path_result_plot.csv', usecols=['position', 'latitude', 'longitude', 'linesag', 'linesway', 'contact', 'strike', 'status'])
+csv_input = pd.read_csv('path_result_plot.csv', usecols=['position', 'latitude', 'longitude', 'linesag', 'linesway', 'contact', 'strike', 'status','income'])
 
 # Sort the DataFrame for "FAILED" status rows in descending order based on the 'strike' column
 failed_rows = csv_input[csv_input['status'] == 'FAILED']
 failed_rows.sort_values(by='strike', ascending=False, inplace=True)
+
+# Find the lowest income value and highlight failed_rows with that value
+lowest_income_value = failed_rows['income'].min()
+for row in failed_rows
+  if failed_rows['income'][row]==lowest_income_value:
+    return ['background-color: yellow'] * len(row)
 
 # Sort the DataFrame for "OK" status rows in descending order based on the 'strike' column
 ok_rows = csv_input[csv_input['status'] == 'OK']

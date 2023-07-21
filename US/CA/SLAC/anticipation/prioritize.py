6b768505b2bd4c5f44b1fb51216f8a9040f806e8
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-21 13:58:37 -0700

    Update prioritize.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/prioritize.py
+++ US/CA/SLAC/anticipation/prioritize.py
@@ -9,13 +9,18 @@ failed_rows.sort_values(by='strike', ascending=False, inplace=True)
 
 # Find the lowest income value and highlight failed_rows with that value
 lowest_income_value = failed_rows['income'].min()
-def highlight_cells(value, color_true, color_false, criteria):
- if value <= criteria:
-     color = color_true 
- else:
-     color = color_false 
- return 'background-color: {}'.format(color)
-failed_rows.style.applymap(highlight_cells, color_true = 'yellow', color_false = 'white', criteria = lowest_income_value)
+def highlight_lowest_income_rows(row):
+    # Create a Series of styles with the same length as the row
+    styles = ['background-color: yellow' if value == lowest_income_value else '' for value in row]
+    return styles
+
+# Use the apply method to apply the highlighting function row-wise to the DataFrame
+highlighted_failed_rows = failed_rows.style.apply(highlight_lowest_income_rows, axis=1)
+highlighted_failed_rows_html = highlighted_failed_rows.render()
+
+# Save the highlighted DataFrame as an HTML file
+with open('highlighted_failed_rows.html', 'w') as f:
+    f.write(highlighted_failed_rows_html)
 
 # Sort the DataFrame for "OK" status rows in descending order based on the 'strike' column
 ok_rows = csv_input[csv_input['status'] == 'OK']

d54659d52b798ac476f7c234df3a26c0d5f7d148
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-21 13:16:40 -0700

    Update prioritize.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/prioritize.py
+++ US/CA/SLAC/anticipation/prioritize.py
@@ -8,11 +8,7 @@ failed_rows = csv_input[csv_input['status'] == 'FAILED']
 failed_rows.sort_values(by='strike', ascending=False, inplace=True)
 
 # Find the lowest income value and highlight failed_rows with that value
-lowest_income_value = failed_rows['income'].min()
-for index, row in failed_rows.iterrows():
-    if row['income'] == lowest_income_value:
-        # Set the background color for rows with the lowest income value to yellow
-        failed_rows.loc[index, :] = 'background_color': 'yellow'
+failed_rows.style.highlight_min(axis=1)
 
 # Sort the DataFrame for "OK" status rows in descending order based on the 'strike' column
 ok_rows = csv_input[csv_input['status'] == 'OK']

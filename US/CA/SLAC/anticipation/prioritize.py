bd34e051e27a371508a39cabfef4d9026d025de6
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-21 14:21:36 -0700

    Update prioritize.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/prioritize.py
+++ US/CA/SLAC/anticipation/prioritize.py
@@ -9,13 +9,13 @@ failed_rows.sort_values(by='strike', ascending=False, inplace=True)
 
 # Find the lowest income value and highlight failed_rows with that value
 lowest_income_value = failed_rows['income'].min()
-def highlight_failed_rows(row):
-    # Create an empty list to hold the styles for the entire row
-    styles = ['background-color: yellow' if value == lowest_income_value else '' for value in row]
+def highlight_row(row):
+    # Create CSS styles for the entire row based on the "highlight" column
+    styles = ['background-color: yellow' if highlight else '' for highlight in row]
     return styles
 
 # Use the apply method to apply the highlighting function row-wise to the DataFrame
-highlighted_failed_rows = failed_rows.style.apply(highlight_failed_rows, axis=1)
+highlighted_failed_rows = failed_rows.style.apply(highlight_row, axis=1)
 
 # Save the highlighted DataFrame as an HTML file
 with open('highlighted_failed_rows.html', 'w') as f:

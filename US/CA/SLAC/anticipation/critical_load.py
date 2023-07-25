7fd5c8069363888101a00a08026b433c65fc6c16
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-25 16:24:14 -0700

    Update critical_load.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/critical_load.py
+++ US/CA/SLAC/anticipation/critical_load.py
@@ -7,15 +7,15 @@ gridlabd.command('network.glm')
 
 services=[]
 
-  objects=gridlabd.get(object)
-  for index,value in objects:
-      if gridlabd.get_class(value)=='meter':
-        parent_obj=gridlabd.get_value(value,parent)
-        if gridlabd.get_class(parent_obj)=='pole':
-          critical_level=gridlabd.get_value(value, service_level)
-          services.append(critical_level)
-        elif:
-          services.append(0)
+objects=gridlabd.get(object)
+for index,value in objects:
+  if gridlabd.get_class(value)=='meter':
+    parent_obj=gridlabd.get_value(value,parent)
+    if gridlabd.get_class(parent_obj)=='pole':
+      critical_level=gridlabd.get_value(value, service_level)
+      services.append(critical_level)
+    elif:
+      services.append(0)
 
 services.to_csv('service_level.csv', index=False)
           

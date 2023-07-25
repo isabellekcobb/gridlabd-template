5726a8b1a20f4b600e8a3abaa56ea9e15b1a3459
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-25 15:32:21 -0700

    Update critical_load.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/critical_load.py
+++ US/CA/SLAC/anticipation/critical_load.py
@@ -1,13 +1,24 @@
 import os
 import gridlabd
+import csv
+import pandas as pd
+
+csv_input=pd.read_csv('path_result_plot.csv')
+services=[]
 def on_finish():
   objects=gridlabd.get(object)
-  meters_list=[]
   for index,value in objects:
       if gridlabd.get_class(value)=='meter':
-        meters_list.append(value)
         parent_obj=gridlabd.get_value(value,parent)
         if gridlabd.get_class(parent_obj)=='pole':
+          critical_level=gridlabd.get_value(value, service_level)
+          services.append(critical_level)
+        elif:
+          services.append(0)
+
+csv_input['service level']=services
+csv_input.to_csv('path_result_plot.csv', index=False)
+          
           
       
         

d884639bf9a7ff6e74e2888b7c9d49c8240fb390
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-25 15:15:45 -0700

    Update critical_load.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/critical_load.py
+++ US/CA/SLAC/anticipation/critical_load.py
@@ -1,1 +1,17 @@
+import os
+import gridlabd
+def on_finish():
+  objects=gridlabd.get(object)
+  meters_list=[]
+  for index,value in objects:
+      if gridlabd.get_class(value)=='meter':
+        meters_list.append(value)
+        parent_obj=gridlabd.get_value(value,parent)
+        if gridlabd.get_class(parent_obj)=='pole':
+          
+      
+        
+          
+          
 
+  

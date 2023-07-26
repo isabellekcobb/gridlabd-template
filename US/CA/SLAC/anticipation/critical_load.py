2dd2214f3e7b1a62196fbbaee75936f843d8502d
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-26 09:59:38 -0700

    Update critical_load.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/critical_load.py
+++ US/CA/SLAC/anticipation/critical_load.py
@@ -1,28 +1,20 @@
-import os
-import gridlabd
-import csv
 import pandas as pd
 
 gridlabd.command('network.glm')
 
-services=[]
+services = []
 
-objects=gridlabd.get('object')
-for index,value in objects:
-  if gridlabd.get_class(value)=='meter':
-    parent_obj=gridlabd.get_value(value,parent)
-    if gridlabd.get_class(parent_obj)=='pole':
-      critical_level=gridlabd.get_value(value, service_level)
-      services.append(critical_level)
+# Use 'meter' as the type argument to get objects of the 'meter' class
+objects = gridlabd.get('meter')
+
+for index, value in objects.items():
+    parent_obj = gridlabd.get_value(value, 'parent')
+    if gridlabd.get_class(parent_obj) == 'pole':
+        critical_level = gridlabd.get_value(value, 'service_level')
+        services.append(critical_level)
     else:
-      services.append(0)
+        services.append(0)
 
-services.to_csv('service_level.csv', index=False)
-          
-          
-      
-        
-          
-          
-
-  
+# Convert the 'services' list to a DataFrame and save it to a CSV file
+df = pd.DataFrame({'service level': services})
+df.to_csv('service_level.csv', index=False)

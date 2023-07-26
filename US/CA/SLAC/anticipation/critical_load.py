21ea38809d7f8852545ed170c20dd9a7954457c7
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-26 10:21:47 -0700

    Update critical_load.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/critical_load.py
+++ US/CA/SLAC/anticipation/critical_load.py
@@ -1,21 +1,28 @@
 import pandas as pd
 import gridlabd
 
+# Define a function to process the data after the simulation is completed
+def process_data():
+    services = []
+
+    # Use 'meter' as the type argument to get objects of the 'meter' class
+    objects = gridlabd.get('meter')
+
+    for index, value in objects.items():
+        parent_obj = gridlabd.get_value(value, 'parent')
+        if gridlabd.get_class(parent_obj) == 'pole':
+            critical_level = gridlabd.get_value(value, 'service_level')
+            services.append(critical_level)
+        else:
+            services.append(0)
+
+    # Convert the 'services' list to a DataFrame and save it to a CSV file
+    df = pd.DataFrame({'service level': services})
+    df.to_csv('service_level.csv', index=False)
+
+# Load the GridLAB-D model (network.glm) and register the process_data() function to run at the end of the simulation
 gridlabd.command('network.glm')
+gridlabd.at("end", process_data)
 
-services = []
-
-# Use 'meter' as the type argument to get objects of the 'meter' class
-objects = gridlabd.get_class('meter')
-
-for index, value in objects.items():
-    parent_obj = gridlabd.get_value(value, 'parent')
-    if gridlabd.get_class(parent_obj) == 'pole':
-        critical_level = gridlabd.get_value(value, 'service_level')
-        services.append(critical_level)
-    else:
-        services.append(0)
-
-# Convert the 'services' list to a DataFrame and save it to a CSV file
-df = pd.DataFrame({'service level': services})
-df.to_csv('service_level.csv', index=False)
+# Start the GridLAB-D simulation
+gridlabd.start()

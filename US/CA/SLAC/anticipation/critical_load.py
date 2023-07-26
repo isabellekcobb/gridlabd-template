2f0bf2eb0e95a49a4084d63194a3377a51c53112
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-26 14:15:36 -0700

    Update critical_load.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/critical_load.py
+++ US/CA/SLAC/anticipation/critical_load.py
@@ -2,26 +2,26 @@ import pandas as pd
 import gridlabd
 
 def on_term():
-    gridlabd.command('network.glm')
-    services = []
+    #gridlabd.command('network.glm')
+    #services = []
     
     # Use 'meter' as the type argument to get objects of the 'meter' class
-    obj_list = gridlabd.get(objects)
+    #obj_list = gridlabd.get(objects)
         
-    for index, value in obj_list.items():
-        if gridlabd.get_class(value) == 'meter':
-            parent_obj = gridlabd.get_value(value, 'parent')
-            if gridlabd.get_class(parent_obj) == 'pole':
-                critical_level = gridlabd.get_value(value, 'service_level')
-                services.append(critical_level)
-            else:
-                services.append(0)
+    #for index, value in obj_list.items():
+    #    if gridlabd.get_class(value) == 'meter':
+    #        parent_obj = gridlabd.get_value(value, 'parent')
+    #        if gridlabd.get_class(parent_obj) == 'pole':
+    #            critical_level = gridlabd.get_value(value, 'service_level')
+     #           services.append(critical_level)
+      #      else:
+     #           services.append(0)
     
     # Convert the 'services' list to a DataFrame and save it to a CSV file
-    df = pd.DataFrame({'service level': services})
-    df.to_csv('service_level.csv', index=False)
+    #df = pd.DataFrame({'service level': services})
+   # df.to_csv('service_level.csv', index=False)
     print('Meter code ran fully')
 
-#on_term()
+on_term()
     
 

0f753706cf9a74bed4640bea465a91c39741ca3e
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-26 10:58:39 -0700

    Update critical_load.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/critical_load.py
+++ US/CA/SLAC/anticipation/critical_load.py
@@ -7,12 +7,12 @@ def process_data():
     services = []
 
     # Use 'meter' as the type argument to get objects of the 'meter' class
-    objects = gridlabd.get_class('meter')
+    objects = gridlabd.get_class('pole')
 
     for index, value in objects.items():
-        parent_obj = gridlabd.get_value(value, 'parent')
-        if gridlabd.get_class(parent_obj) == 'pole':
-            critical_level = gridlabd.get_value(value, 'service_level')
+        child_obj = gridlabd.get_value(value, 'child')
+        if gridlabd.get_class(child_obj) == 'meter':
+            critical_level = gridlabd.get_value(child_obj, 'service_level')
             services.append(critical_level)
         else:
             services.append(0)

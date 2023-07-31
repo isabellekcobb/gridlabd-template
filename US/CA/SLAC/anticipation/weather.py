7797697764e615c44d0d12a743345d178ac41523
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-31 14:04:50 -0700

    Create weather.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

---
+++ US/CA/SLAC/anticipation/weather.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import pvlib
+import requests
+
+def get_nsrdb_data(latitude, longitude):
+    url = f'https://developer.nrel.gov/api/nsrdb/v2/solar/psm3-tmy2?api_key=9zS25U1TomaV3vQzgiAexB8eVaNV02HrrcmtBm0q&lat={latitude}&lon={longitude}&year=2022'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print(f"Failed to fetch NSRDB data for latitude {latitude} and longitude {longitude}.")
+        return None
+
+def main():
+    excel_file = 'test_xls-spida2csv-geodata.xls'
+
+    # Replace 'Sheet1' with the name of the sheet containing the data
+    df = pd.read_excel(excel_file, sheet_name='Design - Pole')
+    gps_points = df['GPS Point']
+
+    for point in gps_points:
+        # Split latitude and longitude from "latitude,longitude" string
+        latitude, longitude = map(float, point.split(','))
+
+        # Get NSRDB data for the given latitude and longitude
+        data = get_nsrdb_data(latitude, longitude)
+
+        # Do something with the NSRDB data
+        # For example, print the data for now
+        if data:
+            print(f"NSRDB Data for latitude {latitude}, longitude {longitude}:")
+            print(data)
+main()

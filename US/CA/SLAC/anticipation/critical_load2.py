b1583a89e912af81b36a66c16ea53dbc073d7129
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-31 11:26:08 -0700

    Update critical_load2.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/critical_load2.py
+++ US/CA/SLAC/anticipation/critical_load2.py
@@ -6,7 +6,7 @@ gridlabd.command('network.glm')
 services = []
     
     # Use 'meter' as the type argument to get objects of the 'meter' class
-obj_list = gridlabd.get_object("pole_configuration_280565E")
+obj_list = gridlabd.get("pole_configuration_280565E")
         
     #for index, value in obj_list.items():
     #    if gridlabd.get_class(value) == 'meter':

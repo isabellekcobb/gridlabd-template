c329f52de69dabd615c2a0108f11a11c1203cef9
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-08-01 14:46:24 -0700

    Update critical_load.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/critical_load.py
+++ US/CA/SLAC/anticipation/critical_load.py
@@ -1,31 +1,27 @@
 import re
 
-def get_meter_objects(glm_file_path):
-    meter_objects = []
+def has_critical_service_level(glm_file_path):
     with open(glm_file_path, 'r') as f:
         glm_content = f.read()
 
         # Use regular expression to find all meter objects with service_level
-        meter_pattern = r"object meter {\s*name (\w+);\s*service_level (HIGH|CRITICAL);"
-        matches = re.findall(meter_pattern, glm_content, re.MULTILINE)
+        meter_pattern = r"object meter {\s*name (\w+);\s*service_level (\w+);"
+        matches = re.finditer(meter_pattern, glm_content)
 
         for match in matches:
-            meter_name, service_level = match
-            meter_objects.append((meter_name, service_level))
+            meter_name, service_level = match.groups()
+            if service_level == "CRITICAL":
+                return True
 
-    return meter_objects
+    return False
 
 def main():
     glm_file_path = '123.glm'  # Replace this with the path to your .glm file
 
-    meter_objects = get_meter_objects(glm_file_path)
-
-    if meter_objects:
-        print("Meter objects with HIGH or CRITICAL service_level:")
-        for meter_name, service_level in meter_objects:
-            print(f"{meter_name}: {service_level}")
+    if has_critical_service_level(glm_file_path):
+        print("Meter object with service_level CRITICAL found in the .glm file.")
     else:
-        print("No meter objects with HIGH or CRITICAL service_level found in the .glm file.")
+        print("No meter object with service_level CRITICAL found in the .glm file.")
 
 if __name__ == "__main__":
     main()

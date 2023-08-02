94ea5dedac8220b5266ba89f458add2dcf6d6442
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-08-02 13:49:51 -0700

    Update critical_load.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/critical_load.py
+++ US/CA/SLAC/anticipation/critical_load.py
@@ -5,12 +5,12 @@ print('check 1')
 def find_meters(input):
 	print('check 3')
 	with open(input,"r") as fh:
+		print('check 4')
 		model=json.load(fh)
 	
 	for obj,data in model['objects'].items():
+		print('check 5')
 		print(model['objects'][obj]['name'])
-
-	print('check 4')
 
 if __name__ == "__main__":
 	print('check 2')

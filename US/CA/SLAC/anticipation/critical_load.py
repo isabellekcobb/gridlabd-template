173ecebf01a0571e6f222a967fad85a6e3635390
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-08-02 14:33:40 -0700

    Update critical_load.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/critical_load.py
+++ US/CA/SLAC/anticipation/critical_load.py
@@ -2,13 +2,19 @@ import sys, os
 import json
 
 def find_meters(input):
-	
+	critical_meters = []
 	with open(input,"r") as fh:
 		model=json.load(fh)
 	
 	for obj,data in model['objects'].items():
 		if data['class'].endswith('meter'):
-			print(model['objects'][obj]['service_level'])
+			if model['objects'][obj]['service_level']=='CRITICAL'
+				critical_meters.append(model['objects'][obj])
+
+	output_file_path = "critical_meters.glm"
+	with open(output_file_path, mode='w') as file:
+    		for line in critical_meters:
+        		file.write(line + "\n")
 
 if __name__ == "__main__":
 	find_meters(sys.argv[1])

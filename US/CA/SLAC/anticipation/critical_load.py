14f8644cae1e5cd702613249c3a84b7ff4200494
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-08-02 15:00:05 -0700

    Update critical_load.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/critical_load.py
+++ US/CA/SLAC/anticipation/critical_load.py
@@ -2,6 +2,11 @@ import sys, os
 import json
 import csv
 
+def write_list_to_csv(data_list, file_name):
+    with open(file_name, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(data_list)
+	    
 def find_meters(input):
 	critical_meters = []
 	with open(input,"r") as fh:
@@ -11,8 +16,8 @@ def find_meters(input):
 		if data['class'].endswith('meter'):
 			if model['objects'][obj]['service_level']=='CRITICAL':
 				critical_meters.append(model['objects'][obj])
-
-	critical_meters.to_csv('critical_meters.csv')
+				
+	write_list_to_csv(critical_meters,'critical_meters.csv')
 
 if __name__ == "__main__":
 	find_meters(sys.argv[1])

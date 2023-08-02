2723f7c13a89fbdb49c7782400d5c915df172aaf
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-08-02 11:27:29 -0700

    Update critical_load.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/critical_load.py
+++ US/CA/SLAC/anticipation/critical_load.py
@@ -1,18 +1,9 @@
 def critical_meters(input_file):
-	#
-	# Load model
-	#
 	with open(input_file,"r") as fh:
 		model = json.load(fh)
-
-	#
-	# Check meter service_level
-	#
         	obj_list=[]
 		for obj,data in model['objects'].items():
 			obj_list.append(obj)
-
 		obj_list.to_csv('objects.csv')
-
 
 critical_meters('123.json')

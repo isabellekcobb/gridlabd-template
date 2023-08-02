0fd66b6dfd2b0b851bf04623ffc7c968cdf8fa4a
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-08-02 14:15:50 -0700

    Update critical_load.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/critical_load.py
+++ US/CA/SLAC/anticipation/critical_load.py
@@ -2,12 +2,13 @@ import sys, os
 import json
 
 def find_meters(input):
+	
 	with open(input,"r") as fh:
 		model=json.load(fh)
 	
 	for obj,data in model['objects'].items():
 		if data['class'].endswith('meter'):
-			print(model['objects'][obj])
+			print(model['objects'][obj]['service_level'])
 
 if __name__ == "__main__":
 	find_meters(sys.argv[1])

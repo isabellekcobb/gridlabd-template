ded1dabec670f99f45470bf9a55722b5bb7bfe67
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-08-02 10:20:26 -0700

    Update critical_load.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/critical_load.py
+++ US/CA/SLAC/anticipation/critical_load.py
@@ -1,38 +1,11 @@
-def extract_meters(input_file_path, output_file_path):
-    critical_high_meters = []
+import gridlabd
+gridlabd.load('123.glm')
 
-    with open(input_file_path, 'r') as input_file:
-        current_object = None
-        is_inside_meter = False
-
-        for line in input_file:
-            if line.strip().lower().startswith("object meter"):
-                is_inside_meter = True
-                current_object = {"object": "meter"}
-
-            if is_inside_meter:
-                parts = line.split('=')
-                if len(parts) == 2:
-                    key = parts[0].strip()
-                    value = parts[1].strip()
-
-                    if key == "service_level" and value in ["CRITICAL", "HIGH"]:
-                        current_object[key] = value
-
-                    if key == "name":
-                        current_object[key] = value
-                        is_inside_meter = False
-                        critical_high_meters.append(current_object)
-
-    with open(output_file_path, 'w') as output_file:
-        for meter in critical_high_meters:
-            output_file.write("object meter;\n")
-            for key, value in meter.items():
-                output_file.write(f"  {key} = {value};\n")
-            output_file.write("\n")
+def extract_meters(output_file_path):
+    critical_meters = gridlabd.get_class('meter')
+    critical_meters.to_csv='output_file_path.csv'
+   
 
 if __name__ == "__main__":
-    input_file_path = "123.glm"
     output_file_path = "critical_meters.glm"
-
-    extract_meters(input_file_path, output_file_path)
+    extract_meters(output_file_path)

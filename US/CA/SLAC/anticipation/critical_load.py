c7db8610f88300be038a73c12344de5909be0c48
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-08-07 11:34:32 -0700

    Update critical_load.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/critical_load.py
+++ US/CA/SLAC/anticipation/critical_load.py
@@ -47,6 +47,26 @@ def find_island(node_name, file_path):
     # If the node_name is not found in any group, it's not assigned to any island.
     return None
 
+def extract_data_from_glm(input_array, glm_file):
+    # Read the entire .glm file into a string
+    with open(glm_file, 'r') as file:
+        glm_data = file.read()
+
+    extracted_data = {}
+
+    # Loop through each object name in the input array
+    for object_name in input_array:
+        # Create a regular expression pattern to match the object and its data
+        pattern = r"\sobject\s" + re.escape(object_name) + r"\s*{(.*?)}"
+
+        # Use re.findall() to find all matches of the pattern in the .glm data
+        matches = re.findall(pattern, glm_data, re.DOTALL)
+
+        # If matches were found, store the data associated with the object name
+        if matches:
+            extracted_data[object_name] = matches[0]
+
+    return extracted_data
 
 def find_meters(input):
 	critical_meters = []
@@ -61,8 +81,9 @@ def find_meters(input):
 				critical_island=find_island(model['objects'][obj]['parent'], 'groups.glm')
 				objs=extract_objects('groups.glm', critical_island)
 				critical_objs.append(objs)
-	write_list_to_glm(critical_objs,'critical_objs.glm')
+	return critical_objs
 
 input_glm=sys.argv[1]
 if __name__ == "__main__":
-	find_meters(input_glm)
+	input_array=find_meters(input_glm)
+	extract_data_from_glm(input_array, input_glm)

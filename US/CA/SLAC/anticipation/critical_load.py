fedf2fb56d38e12f0322bb9ad721c97f4003dd4d
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-08-11 10:52:42 -0700

    Update critical_load.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/critical_load.py
+++ US/CA/SLAC/anticipation/critical_load.py
@@ -63,9 +63,9 @@ def find_meters(input):
 				critical_objs.append(extract_objects('groups.glm', critical_island))
 				for obj_properties in critical_objs:
 					print('work goddamn')
-    				key = tuple(obj_properties)
+    					key = tuple(obj_properties)
     				if key in model['objects']:
-						print('this better work chanel')
+					print('this better work chanel')
         				critical_data.append(model['objects'][key])
     				else:
         				print(f"Object not found: {key}")

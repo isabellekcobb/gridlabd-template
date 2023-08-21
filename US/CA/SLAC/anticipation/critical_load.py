1cd6a2b7f7d51e7ded25980fc268a4e0317013fc
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-08-21 14:31:14 -0700

    Update critical_load.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/critical_load.py
+++ US/CA/SLAC/anticipation/critical_load.py
@@ -62,6 +62,7 @@ def find_meters(input):
 	for obj,data in model['objects'].items():
 		for x in critical_objs:
 			print(critical_objs[1])
+			print('new test')
 			if 'name' in data and data['name']==critical_objs[x]:
 				print('did it get here?')
 				critical_data.append(model['objects'][obj])

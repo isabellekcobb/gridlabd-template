19e2b4de2c9bd7d97947b21a1d3d508677c59662
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-08-22 10:41:48 -0700

    Update prioritize.py
    Signed-off-by: isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>

--- US/CA/SLAC/anticipation/prioritize.py
+++ US/CA/SLAC/anticipation/prioritize.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 # Read the CSV file and use the 'status' column as the sort key
 csv_input = pd.read_csv('path_result_plot.csv', usecols=['position', 'latitude', 'longitude', 'linesag', 'linesway', 'contact', 'strike', 'status','income'])
@@ -6,6 +7,8 @@ csv_input = pd.read_csv('path_result_plot.csv', usecols=['position', 'latitude',
 strike = csv_input['strike']
 contact = csv_input['contact']
 income = csv_input['income']
+lat = csv_input['latitude']
+lon = csv_input['longitude']
 
 length=len(csv_input['strike'])
 strike_pts = pd.Series(0, index=range(length))
@@ -31,10 +34,42 @@ for index, value in income.items():
     else:
         income_pts[index] = 0
 
+# critical loading
+
+def parse_glm_file(file_path):
+    with open(file_path, 'r') as file:
+        data = file.read()
+
+    objects = re.findall(r'\{([^}]*)\}', data)
+    object_dicts = []
+
+    for obj in objects:
+        obj_data = {}
+        lines = obj.strip().split('\n')
+        for line in lines:
+            key, value = map(str.strip, line.split(':', 1))
+            obj_data[key] = value
+        object_dicts.append(obj_data)
+
+    return object_dicts
+
+def extract_lat_lon(obj_data):
+    latitude = obj_data.get('latitude', None)
+    longitude = obj_data.get('longitude', None)
+    return latitude, longitude
+
+latlong_list = parse_glm_file('critical_data.glm')
+    
 load_pts = pd.Series(0, index=range(length))
 csv_input['critical load'] = [False] * length
-
 loads = csv_input['critical load']
+
+for obj_data in latlong_list:
+    latitude, longitude = extract_lat_lon(obj_data)
+    for index, value in loads.items():
+        if lat[index]==latitude and lon[index]==longitude:
+            loads[index]=True
+
 # Assign poles attached to critical loads a critical value of 5, leave all others with zero point value
 for index, value in loads.items():
     if value == True:
